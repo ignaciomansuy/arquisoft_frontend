58e66f524722d5f595dc191eeae42e219dd65c0d
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-05-25 11:23:09 -0400

    feat(auth): hook for adding user_id to user_metadata working properly

---
+++ src/hooks/auth/updateUserId.js
@@ -0,0 +1,31 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+const updateMetadata = async () => {
+  const domain = "arqui-soft-grupo09.us.auth0.com";
+
+  const accessToken = await getAccessTokenSilently({
+    audience: `https://${domain}/api/v2/`,
+    scope: "read:current_user",
+  });
+
+  const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+  const metadataResponse = await fetch(userDetailsByIdUrl, {
+    method: "PATCH",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user_metadata: {
+        user_id: 1,
+      },
+    })
+  });
+
+  const { user_metadata } = await metadataResponse.json();
+  return user_metadata;
+}
+
+export default updateMetadata;
+
--- src/pages/user/AuthProfile.jsx
+++ src/pages/user/AuthProfile.jsx
@@ -4,11 +4,10 @@ import { useAuth0 } from "@auth0/auth0-react";
 const Profile = () => {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
   const [userMetadata, setUserMetadata] = useState(null);
+  const domain = "arqui-soft-grupo09.us.auth0.com";
 
   useEffect(() => {
     const getUserMetadata = async () => {
-      const domain = "arqui-soft-grupo09.us.auth0.com";
-  
       try {
         const accessToken = await getAccessTokenSilently({
           audience: `https://${domain}/api/v2/`,
