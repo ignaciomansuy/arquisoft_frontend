1cae18f56162fac2f76b7169214250a24b142879
Coolgatty <coolgatty@gmail.com>
2022-06-22 01:41:45 -0400

    added chatbox that can render messages. no real functionality yet

--- src/pages/chat/show.page.jsx
+++ src/pages/chat/show.page.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
-import { Typography, Box } from '@mui/material';
-import Hero from '../../components/layout/hero.component';
-import Map from '../../components/map.component';
+import './chat.css'
+import { Box } from '@mui/material';
+// RCE CSS
+import "react-chat-elements/dist/main.css";
+// MessageBox component
+import { MessageBox, MessageList, Input, Button } from "react-chat-elements";
 
-export default function ShowMapPage() {
+
+
+// // Clear text, e.g.:
+
+// inputClear = () => { };
+// // ...
+// <Input clear={(clear) => (inputClear = clear)} placeholder="Type here..." />;
+// // ...
+// inputClear();
+
+export default function ShowChatPage() {
+
+  const inputReferance = React.createRef();
+  const messageListReferance = React.createRef();
+  const messagesList = [];
+  const [messages, setMessages] = React.useState(messagesList);
+  let inputClear = () => { inputReferance.current.value = '' };
+
+  const handleSendMessage = (text) => {
+    text = text.replace(/(\r\n|\n|\r)/gm, '');
+    if (text === '' || text.length === 0) {
+      inputClear();
+      return;
+    }
+    const newList = messages.concat({
+      position: 'right',
+      type: 'text',
+      text: text,
+      date: new Date(),
+    });
+    setMessages(newList);
+    inputClear();
+  }
+
+
   return (
-<div></div>
+    <div className='Box'>
+      <Box sx={{ my: 2 }}>
+        <MessageList
+          referance={messageListReferance}
+          className='message-list'
+          lockable={true}
+          toBottomHeight={'100%'}
+          dataSource={messages} />
+
+        <Input
+          referance={inputReferance}
+          placeholder="Type here..."
+          multiline={true}
+          clear={(clear) => (inputClear = clear)}
+          rightButtons={<Button color="white" backgroundColor="black" text="Send" onClick={() => handleSendMessage(inputReferance.current.value)} />}
+          onKeyPress={(e) => {
+            if (e.charCode === 13) {
+              handleSendMessage(inputReferance.current.value);
+            }
+          }}
+        />
+
+      </Box></div>
   );
 }
