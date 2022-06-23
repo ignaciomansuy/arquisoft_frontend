01c15f8350fe221a885691af5d8f107f58cf81f0
Coolgatty <coolgatty@gmail.com>
2022-06-22 22:35:11 -0400

    finished basic offline chat functionality

--- src/pages/chat/show.page.jsx
+++ src/pages/chat/show.page.jsx
@@ -19,10 +19,17 @@ import { MessageBox, MessageList, Input, Button } from "react-chat-elements";
 export default function ShowChatPage() {
 
   const inputReferance = React.createRef();
+
   const messageListReferance = React.createRef();
+
   const messagesList = [];
   const [messages, setMessages] = React.useState(messagesList);
+
   let inputClear = () => { inputReferance.current.value = '' };
+
+  let sortByDate = (item, other) => {
+    return item['date'] < other['date']
+  }
 
   const handleSendMessage = (text) => {
     text = text.replace(/(\r\n|\n|\r)/gm, '');
@@ -40,30 +47,53 @@ export default function ShowChatPage() {
     inputClear();
   }
 
+  const handleReceiveMessage = (text) => {
+    text = text.replace(/(\r\n|\n|\r)/gm, '');
+    if (text === '' || text.length === 0) {
+      inputClear();
+      return;
+    }
+    const newList = messages.concat({
+      position: 'left',
+      type: 'text',
+      text: text,
+      date: new Date(),
+    });
+    setMessages(newList);
+    inputClear();
+  }
+
 
   return (
-    <div className='Box'>
-      <Box sx={{ my: 2 }}>
-        <MessageList
-          referance={messageListReferance}
-          className='message-list'
-          lockable={true}
-          toBottomHeight={'100%'}
-          dataSource={messages} />
+    <div className='box'>
+      <div className='grid'>
+        <div>
+          <MessageList
+            referance={messageListReferance}
+            className='message-list'
+            lockable={true}
+            toBottomHeight={'100%'}
+            dataSource={messages.sort(sortByDate)} />
+        </div>
+        <div>
+          <Input
+            referance={inputReferance}
+            placeholder="Type here..."
+            multiline={true}
+            clear={(clear) => (inputClear = clear)}
+            rightButtons={[
+              <Button color="white" backgroundColor="black" text="Send" onClick={() => handleSendMessage(inputReferance.current.value)} />,
+              <Button color="white" backgroundColor="black" text="Receive (Debug)" onClick={() => handleReceiveMessage(inputReferance.current.value)} />
+            ]}
+            onKeyPress={(e) => {
+              if (e.charCode === 13) {
+                handleSendMessage(inputReferance.current.value);
+              }
+            }}
+          />
 
-        <Input
-          referance={inputReferance}
-          placeholder="Type here..."
-          multiline={true}
-          clear={(clear) => (inputClear = clear)}
-          rightButtons={<Button color="white" backgroundColor="black" text="Send" onClick={() => handleSendMessage(inputReferance.current.value)} />}
-          onKeyPress={(e) => {
-            if (e.charCode === 13) {
-              handleSendMessage(inputReferance.current.value);
-            }
-          }}
-        />
-
-      </Box></div>
+        </div>
+      </div>
+    </div>
   );
 }
